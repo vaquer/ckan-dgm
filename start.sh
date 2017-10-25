5a06bac9a68bf9721256120c280c970bcc210c6f
vaquer <akura11.tt@gmail.com>
2017-10-24 22:00:15 -0500

    Desactivando el harvester del frontend

--- start.sh
+++ start.sh
@@ -12,9 +12,8 @@ sed -i -e "s|ckan.datapusher.url = http://0.0.0.0:8800/|ckan.datapusher.url = $D
 
 sed -i -e "s|hostname:port:database:username:password|$POSTGRES_PORT_5432_TCP_ADDR:5432:$POSTGRES_ENV_POSTGRES_DB:$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD|" /root/.pgpass
 # Redis Configs
-sed -i -e "s|ckan.harvest.mq.hostname = hostharvest|ckan.harvest.mq.hostname = $REDIS_PORT_6379_TCP_ADDR|" /project/development.ini
-sed -i -e "s|ckan.harvest.mq.port = 6379|ckan.harvest.mq.port = $REDIS_PORT|" /project/development.ini
-sed -i -e "s|mxtheme.adela_api_endopint = |mxtheme.adela_api_endopint = $ADELA_ENDPOINT|" /project/development.ini
+# sed -i -e "s|ckan.harvest.mq.hostname = hostharvest|ckan.harvest.mq.hostname = $REDIS_PORT_6379_TCP_ADDR|" /project/development.ini
+# sed -i -e "s|ckan.harvest.mq.port = 6379|ckan.harvest.mq.port = $REDIS_PORT|" /project/development.ini
 
 $CKAN_HOME/bin/paster --plugin=ckan datastore set-permissions -c /project/development.ini
 
@@ -25,9 +24,9 @@ if [ "$INIT_DBS" = true ]; then
   $CKAN_HOME/bin/paster --plugin=ckanext-spatial spatial initdb 4326 -c /project/development.ini
 fi
 
-if [ "$INIT_HARVEST" = true ]; then
-    $CKAN_HOME/bin/paster --plugin=ckanext-harvest harvester initdb -c /project/development.ini
-fi
+# if [ "$INIT_HARVEST" = true ]; then
+    # $CKAN_HOME/bin/paster --plugin=ckanext-harvest harvester initdb -c /project/development.ini
+# fi
 # Load a dump file to ckan database
 # Temporalmente deshabilitado para probar el dump
 
